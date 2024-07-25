b575cefec70ae7e9ad5352f280b7930e0382028b
Matheus Costa <119532296+timelessgenius@users.noreply.github.com>
2024-07-25 09:23:00 -0300

    .

---
+++ 1772A_codeforces.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int a, b; 
+        char c; 
+        cin >> a >> c >> b;
+
+        cout << a+b << endl;
+    }
+
+    return 0;
+}
